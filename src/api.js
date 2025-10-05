23858c77d749479c5978791b90e272987ee1e64d
RahulShukla387 <KnowMeTechnical@gmail.com>
2025-10-06 03:08:31 +0530

    added in api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,37 @@
+// // src/api.js
+// const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+// export const apiFetch = async (endpoint, options = {}) => {
+//   const response = await fetch(`${BACKEND_URL}${endpoint}`, {
+//     headers: {
+//       "Content-Type": "application/json",
+//       ...(options.headers || {})
+//     },
+//     ...options
+//   });
+
+//   if (!response.ok) {
+//     const error = await response.text();
+//     throw new Error(error || "API request failed");
+//   }
+
+//   return response.json();
+// };
+
+
 // src/api.js
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const apiFetch = async (endpoint, options = {}) => {
+  const token = localStorage.getItem("token");
+
   const response = await fetch(`${BACKEND_URL}${endpoint}`, {
     headers: {
       "Content-Type": "application/json",
-      ...(options.headers || {})
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      ...(options.headers || {}),
     },
-    ...options
+    ...options,
   });
 
   if (!response.ok) {
